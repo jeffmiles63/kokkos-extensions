d3110986f14cd93411d9fd834eb200bbfbab7fa3
jeffmiles63 <jsmiles@apollo.sandia.gov>
2020-07-01 17:00:06 -0600

    update pooled unit test, and fix issue with Serial2 and Cuda2

--- UmpireSpace/core/unit_test/TestUmpireAllocators.hpp
+++ UmpireSpace/core/unit_test/TestUmpireAllocators.hpp
@@ -19,70 +19,115 @@ struct TestUmpireAllocators {
   using mem_space_device = Kokkos::UmpireSpace<default_device>;
   using host_view_type   = Kokkos::View<T*, mem_space_host>;
   using device_view_type = Kokkos::View<T*, mem_space_device>;
-  using view_ctor_prop_device =
-      Kokkos::Impl::ViewCtorProp<std::string, mem_space_device>;
-  using view_ctor_prop_host =
-      Kokkos::Impl::ViewCtorProp<std::string, mem_space_host>;
 
   void run_tests() {
     // no pooling
     //
-    mem_space_host no_alloc_host;
-    mem_space_device no_alloc_device;
+    {
+       mem_space_host no_alloc_host;
+       mem_space_device no_alloc_device;
 
-    // direct memory allocation (not really a case we should share...)
-    double* ptr = (double*)no_alloc_device.allocate(N * sizeof(double));
-    Kokkos::parallel_for(
-        Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, N),
-        KOKKOS_LAMBDA(const int i) { ptr[i] = (double)i; });
+       // direct memory allocation (not really a case we should share...)
+       double* ptr = (double*)no_alloc_device.allocate(N * sizeof(double));
+       Kokkos::parallel_for(
+          Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, N),
+          KOKKOS_LAMBDA(const int i) { ptr[i] = (double)i; });
 
-    // allocate views and mirrors
-    device_view_type v1(view_ctor_prop_device("v1", no_alloc_device), N);
-    host_view_type v2(view_ctor_prop_host("v2", no_alloc_host), N);
+       // allocate views and mirrors
+       device_view_type v1(Kokkos::view_alloc("v1", no_alloc_device), N);
+       host_view_type v2(Kokkos::view_alloc("v2", no_alloc_host), N);
 
-    auto h_v1 = Kokkos::create_mirror(Kokkos::HostSpace(), v1);
-    auto h_v2 = Kokkos::create_mirror(Kokkos::HostSpace(), v2);
+       auto h_v1 = Kokkos::create_mirror(Kokkos::HostSpace(), v1);
+       auto h_v2 = Kokkos::create_mirror(Kokkos::HostSpace(), v2);
 
-    // initialize host mirrors and copy to umpire space
-    for (int i = 0; i < N; i++) {
-      h_v1(i) = i;
-      h_v2(i) = i * 2;
+       // initialize host mirrors and copy to umpire space
+       for (int i = 0; i < N; i++) {
+          h_v1(i) = i;
+          h_v2(i) = i * 2;
+       }
+       Kokkos::deep_copy(v1, h_v1);
+       Kokkos::deep_copy(v2, h_v2);
+
+       // Execute kernels one on device and one on host
+       Kokkos::parallel_for(
+           Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, N),
+           KOKKOS_LAMBDA(const int i) { v1(i) *= 2; });
+       Kokkos::fence();
+       Kokkos::parallel_for(
+           Kokkos::RangePolicy<Kokkos::DefaultHostExecutionSpace>(0, N),
+           KOKKOS_LAMBDA(const int i) { v2(i) *= 2; });
+
+       // copy back to host and validate
+       Kokkos::deep_copy(h_v1, v1);
+       Kokkos::deep_copy(h_v2, v2);
+
+       for (int i = 0; i < N; i++) {
+         ASSERT_EQ(h_v1(i), 2 * i);
+         ASSERT_EQ(h_v2(i), i * 4);
+       }
     }
-    Kokkos::deep_copy(v1, h_v1);
-    Kokkos::deep_copy(v2, h_v2);
+    {
+       // pooled allocator - host
+       //
+       mem_space_host::make_new_allocator<umpire::strategy::DynamicPool>("_pool");
+       mem_space_host pooled_host("HOST_pool");
+       double* ptrII = (double*)pooled_host.allocate(N * sizeof(double));
+       pooled_host.deallocate(ptrII, N * sizeof(double));
 
-    // Execute kernels one on device and one on host
-    Kokkos::parallel_for(
-        Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, N),
-        KOKKOS_LAMBDA(const int i) { v1(i) *= 2; });
-    Kokkos::fence();
-    Kokkos::parallel_for(
-        Kokkos::RangePolicy<Kokkos::DefaultHostExecutionSpace>(0, N),
-        KOKKOS_LAMBDA(const int i) { v2(i) *= 2; });
+       host_view_type v2(Kokkos::view_alloc("v2", pooled_host), N);
 
-    // copy back to host and validate
-    Kokkos::deep_copy(h_v1, v1);
-    Kokkos::deep_copy(h_v2, v2);
+       auto h_v2 = Kokkos::create_mirror(Kokkos::HostSpace(), v2);
 
-    for (int i = 0; i < N; i++) {
-      ASSERT_EQ(h_v1(i), 2 * i);
-      ASSERT_EQ(h_v2(i), i * 4);
+       // initialize host mirrors and copy to umpire space
+       for (int i = 0; i < N; i++) {
+          h_v2(i) = i * 2;
+       }
+       Kokkos::deep_copy(v2, h_v2);
+       Kokkos::parallel_for(
+           Kokkos::RangePolicy<Kokkos::DefaultHostExecutionSpace>(0, N),
+           KOKKOS_LAMBDA(const int i) { v2(i) *= 2; });
+
+       // copy back to host and validate
+       Kokkos::deep_copy(h_v2, v2);
+
+       for (int i = 0; i < N; i++) {
+         ASSERT_EQ(h_v2(i), i * 4);
+       }
     }
-    // pooled allocator
-    //
-    mem_space_host::make_new_allocator<umpire::strategy::DynamicPool>("_pool");
-    mem_space_host pooled_host("HOST_pool");
-    double* ptrII = (double*)pooled_host.allocate(N * sizeof(double));
-    pooled_host.deallocate(ptrII, N * sizeof(double));
-    
-    mem_space_device::make_new_allocator<umpire::strategy::DynamicPool>("_pool");
-    mem_space_device pooled_dev("DEVICE_pool");
-    double* ptrIII = (double*)pooled_dev.allocate(N * sizeof(double));
-    pooled_dev.deallocate(ptrIII, N * sizeof(double));
+    {
+       // pooled allocator - device
+       mem_space_device::make_new_allocator<umpire::strategy::DynamicPool>(
+           "_pool");
+       mem_space_device pooled_dev("DEVICE_pool");
+       double* ptrIII = (double*)pooled_dev.allocate(N * sizeof(double));
+       pooled_dev.deallocate(ptrIII, N * sizeof(double));
 
+       // allocate views and mirrors
+       device_view_type v1(Kokkos::view_alloc("v1", pooled_dev), N);
+
+       auto h_v1 = Kokkos::create_mirror(Kokkos::HostSpace(), v1);
+
+       // initialize host mirrors and copy to umpire space
+       for (int i = 0; i < N; i++) {
+          h_v1(i) = i;
+       }
+       Kokkos::deep_copy(v1, h_v1);
+
+       // Execute kernels one on device and one on host
+       Kokkos::parallel_for(
+           Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, N),
+           KOKKOS_LAMBDA(const int i) { v1(i) *= 2; });
+       
+       // copy back to host and validate
+       Kokkos::deep_copy(h_v1, v1);
+
+       for (int i = 0; i < N; i++) {
+         ASSERT_EQ(h_v1(i), 2 * i);
+       }
+    }
     // typed allocator
     //
-    printf("tests complete, let the descoping begin...\n");
+       printf("tests complete, let the descoping begin...\n");
   }
 };
 
